8d34fba5054c1f9d4bfc5277ff679a5f2e7f0ad7
Vladislav Trofimov <arda.yug@gmail.com>
2022-06-21 23:24:43 +0300

    add Example002_HelloUser

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,13 @@
+﻿// Вывод текста в столбик
+
+// Console.WriteLine("Введите Ваше имя ");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет! ");
+// Console.WriteLine(username);
+
+// Вывод текста в строку
+
+Console.Write("Введите Ваше имя ");
+string username = Console.ReadLine();
+Console.Write("Привет! ");
+Console.Write(username);
