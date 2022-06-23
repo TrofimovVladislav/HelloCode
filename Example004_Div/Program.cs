7d4edffefd66d5c008f1012713541adecd679a37
Vladislav Trofimov <arda.yug@gmail.com>
2022-06-23 19:24:23 +0300

    Add Div operation

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,11 @@
+﻿// Деление целых чисел без остатка
+
+// int numberA =10;
+// int numberB = 5;
+// Console.WriteLine(numberA / numberB);
+
+// Деление чисел с дробным ответом
+
+double numberA =12;
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
