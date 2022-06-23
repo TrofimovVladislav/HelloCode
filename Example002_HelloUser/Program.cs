2b8950353d9190778a5eebd31f27ad4aca908bcb
Vladislav Trofimov <arda.yug@gmail.com>
2022-06-23 18:24:53 +0300

    Fix commit

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -7,9 +7,8 @@
 
 // Вывод текста в строку
 
-Console.Write("Введите Ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет! ");
-Console.Write(username );
-
-
+        Console.Write("Введите Ваше имя ");
+        string username = Console.ReadLine();
+        Console.Write("Привет! ");
+        Console.Write(username);
+        
