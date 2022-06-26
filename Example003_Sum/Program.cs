0be16ecddd864b6975b898fc5d834b8523d98ae4
Vladislav Trofimov <arda.yug@gmail.com>
2022-06-26 19:40:53 +0300

    Add comment to str 8,9

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,7 +1,12 @@
-﻿// int numberA = 3;
+﻿// Нахождение суммы заданных чисел из типа данных int
+
+// int numberA = 3;
 // int numberB = 5;
 // int result = numberA + numberB;
 // Console.WriteLine(result);
+
+// Нахождение суммы случайных чисел
+// Random(); - генератор случайных чисел, Next(min, max) - целоечисло из диапазона min - max-1;
 
 int numberA = new Random().Next(1, 10); // 1, 2, 3, 4, 5 .... 9
 Console.WriteLine(numberA);
