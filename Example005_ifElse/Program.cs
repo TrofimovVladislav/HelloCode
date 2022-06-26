5cd4a05a33c0b020cca86066c7fc21bd77c42ce6
Vladislav Trofimov <arda.yug@gmail.com>
2022-06-26 19:56:59 +0300

    Add comment to str 1-6

--- Example005_ifElse/Program.cs
+++ Example005_ifElse/Program.cs
@@ -1,12 +1,17 @@
-﻿Console.Write("Введите имя пользователя: ");
-string username = Console.ReadLine(); // string - это обозначение типа данных, username - заданнон нами имя переменной
-
-if(username == "Маша")
+﻿// string - это обозначение типа данных - строка, username - заданное нами имя переменной.
+// if(username == "маша")
+// При такой записи имени пользователя с большой буквы, для его иднтификации возможны сложности
+// из-за того, что пользователь может вводить своё иимя без учета регистра
+// Во избежания такой сложности, лучше писать например: username.ToLower()
+// и имя ожидаемого пользователя с маленькой буквы, в нашем случае: "маша".
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша") 
 {
     Console.WriteLine("УРА!!! ЭТО ЖЕ МАША!!!");
 }
 else
 {
     Console.Write("Привет, ");
-    Console.WriteLine(username);
+    Console.WriteLine(username + "!");
 }
