f46df8e185d1ce793baff69711a709d76ca02663
Vladislav Trofimov <arda.yug@gmail.com>
2022-07-12 08:10:21 +0300

    Add control commit

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -4,31 +4,31 @@
 // Т.е. Этот метод что-то выполняет (в данном случае тело метода не описано)
 // и может в конце его исполнения вывести, например, "Имя Автора".
 
-void Method1()
-{
-    Console.WriteLine("Имя Автора");
-}
+// void Method1()
+// {
+//     Console.WriteLine("Имя Автора");
+// }
 
 // Method1(); // Команда на вывод метода
 // ----------------------------------------------------------------------------
 // Тип 2: Метод может принимать какаие-то аргументы, но ничего не возвращают.
 
-void Method2(string msg)  // string - ТипДанных (в данном случае - строка(string)), msg - Аргумент. 
-{
-    Console.WriteLine(msg);
-}
+// void Method2(string msg)  // string - ТипДанных (в данном случае - строка(string)), msg - Аргумент. 
+// {
+//     Console.WriteLine(msg);
+// }
 
 // Method2("Текст сообщения");   
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine("Текст");
-        i++;
-    }
-}
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i < count)
+//     {
+//         Console.WriteLine("Текст");
+//         i++;
+//     }
+// }
 //  Method21(msg: "Текст", count: 4); 
 
 /* Именованные ргументы, т.е. указываемое, значение "Текст" 
@@ -44,10 +44,10 @@ void Method21(string msg, int count)
 /* Если метод что-то возвращает, обязательно указываем тип данных
 которые он возвращает */
 
-int Method3()
-{
-    return DateTime.Now.Year;  // Это тело метода
-}
+// int Method3()
+// {
+//     return DateTime.Now.Year;  // Это тело метода
+// }
 
 // int year = Method3();   // Объявляем переменную в которую будет возвращен результат работы метода.
 // Console.WriteLine(year);  // Выводим значение переменной на экран.
@@ -84,15 +84,15 @@ for(int i = 0; i < 10; i++)
 
 // Метод 4 с использованием цикла for (вместо цикла while) 
 
-string Method4(int count, string text)
-{
-    string result = String.Empty;
-    for (int i = 0; i < count; i++)
-    {
-        result = result + text;
-    }
-    return result;
-}
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
 
 // string res = Method4(10, "Строка");
 // Console.WriteLine(res);
@@ -113,11 +113,12 @@ string Method4(int count, string text)
 заменить на большие буаквы К, а все большие буквы С - заменить на маленькие буквы с.
 */
 
+/*
 string text = "— Я думаю, — сказал князь, улыбаясь,"
             + "— что, ежели бы ваС послали вместо нашего милого Винценгероде,"
             + "вы бы взяли приступом согласие прусского короля. Вы так красноречивы."
             + "Вы дадите мне чаю?";
-
+*/
 
 // string s = "qwerty'
 //             012345 - Поясняем, что в строке первыйсимвол - 0, второй - 1 и т.д
@@ -156,23 +157,23 @@ void PrintArray(int[] array)
     Console.WriteLine();
 }
 
-void SelectionSort(int[] array)
-{
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int minPosition = i;
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
 
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if (array[j] < array[minPosition]) minPosition = j;
-        }
-        int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
-    }
-}
-PrintArray(array);
-SelectionSort(array);
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+// PrintArray(array);
+// SelectionSort(array);
 
 PrintArray(array);
 
